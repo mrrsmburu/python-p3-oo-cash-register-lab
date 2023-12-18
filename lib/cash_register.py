1c3309943e6f1aa8d7a93b5ca54a5356d36317db
mrrsmburu <mrrsmburu@gmail.com>
2023-12-18 10:52:20 +0300

    completed assignment

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    
+    def __init__(self, price, discount):
+        self.price = price
+        self.discount = discount
+
+    def apply_discount(self):
+        discounted_price = self.price - (self.price * self.discount / 100)
+        return float(discounted_price)  
+
+
+if __name__ == "__main__":
+    
+    product_instance = Product(price=100, discount=20)
+
+    
+    discounted_price = product_instance.apply_discount()
+    print(f"Discounted Price: {discounted_price}")
+
+  
